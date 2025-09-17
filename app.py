a4450e669d01d8aa3a55fe85353e4ffcb2bed8dd
Loris Polenz <contact@lorispolenz.com>
2025-09-17 21:24:09 +0200

    feat(app): improve app flow and reduce unnecessary git pulls

--- app.py
+++ app.py
@@ -27,6 +27,9 @@ CLIENT_SECRET = os.getenv("CLIENT_SECRET")
 GIT_REPO_URL = os.getenv("GIT_REPO_URL")
 GIT_REPO_PATH = os.getenv("GIT_REPO_PATH")
 
+# Global variables
+UPDATED_FILES = []
+EMPTY_FOLDERS = []
 
 # Init Microsoft Graph client
 scopes = ['https://graph.microsoft.com/.default']
@@ -56,7 +59,8 @@ async def map_children(drive_id: str, parent_item, path=str):
     res = await graph_client.drives.by_drive_id(drive_id).items.by_drive_item_id(parent_item.id).children.get()
 
     if len(res.value) == 0:
-        await create_empty_folder(path)
+        logging.info(f"Empty folder detected: {path}")
+        EMPTY_FOLDERS.append(path)
         return
 
     for item in res.value:
@@ -67,24 +71,34 @@ async def map_children(drive_id: str, parent_item, path=str):
                 continue
 
             logging.info(f"{path}/{item.name} ({item.file.mime_type})")
-            await create_file(f"{path}/{item.name}", item.id)
+            UPDATED_FILES.append({
+                "path": f"{path}/{item.name}",
+                "id": item.id
+            })
+
         if item.folder:
             await map_children(drive_id, item, path=path + '/' + item.name)
 
 
-async def main():
+def fetch_repo() -> Repo:
+    logging.info("Setting up Git repository...")
     # Init git repository
-    if not os.path.exists("knowledge_base"):
-        logging.info("Cloning repository...")
-
-        # Clone the repository using the custom SSH key
-        GIT_REPO = Repo.clone_from(GIT_REPO_URL, GIT_REPO_PATH)
-    else:
+    if os.path.exists(GIT_REPO_PATH):
         GIT_REPO = Repo(GIT_REPO_PATH)
 
         logging.info("Pulling latest changes...")
         GIT_REPO.remotes.origin.pull()
+    else:
+        logging.info("Cloning repository...")
 
+        # Clone the repository using the custom SSH key
+        GIT_REPO = Repo.clone_from(GIT_REPO_URL, GIT_REPO_PATH)
+
+    return GIT_REPO
+
+
+async def main():
+    logging.info("Starting the synchronization process...")
     root_result = await graph_client.drives.by_drive_id(DRIVE_ID)\
         .items.by_drive_item_id(ROOT_DIR_ID).children.get()
 
@@ -95,6 +109,20 @@ async def main():
 
         if item.folder:
             await map_children(DRIVE_ID, item, path=f"./{GIT_REPO_PATH}/{item.name}")
+
+    if len(UPDATED_FILES) == 0 and len(EMPTY_FOLDERS) == 0:
+        logging.info("No changes detected in the last 15 minutes. Exiting...")
+        return
+
+    GIT_REPO = fetch_repo()
+
+    for file in UPDATED_FILES:
+        logging.info(f"Processing file: {file['path']}")
+        await create_file(file['path'], file['id'])
+
+    for folder in EMPTY_FOLDERS:
+        logging.info(f"Processing empty folder: {folder}")
+        await create_empty_folder(folder)
 
     untracked_files = len(GIT_REPO.untracked_files)
     changed_files = len(GIT_REPO.index.diff(None))
